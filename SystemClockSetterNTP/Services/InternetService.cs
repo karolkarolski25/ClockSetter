fd494fc6175177d5f0e37efa91830459e0e8a7ea
KarolZarebski <karolkarolski25@gmail.com>
2020-10-11 01:02:36 +0200

    Added checking internet connection

---
+++ SystemClockSetterNTP/Services/InternetService.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Extensions.Logging;
+using System.Net;
+
+namespace SystemClockSetterNTP.Services
+{
+    public class InternetService : IInternetService
+    {
+        private readonly ILogger<InternetService> _logger;
+
+        public InternetService(ILogger<InternetService> logger)
+        {
+            _logger = logger;
+        }
+
+        public bool IsInternetConnectionAvailable()
+        {
+            try
+            {
+                var s = new WebClient().OpenRead("http://google.com/generate_204");
+
+                _logger.LogDebug("Internet connection available");
+
+                return true;
+            }
+            catch
+            {
+                _logger.LogDebug("Internet connection unavailable");
+
+                return false;
+            }
+        }
+    }
+}
